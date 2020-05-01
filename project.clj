ed702fb00a1fa9f4fef2a469a0e6ac32e53ea563
Jordan Yee <jyee@qanaq.net>
2020-05-01 14:45:16 -0600

    Update project.clj for clj/cljs paths

--- project.clj
+++ project.clj
@@ -1,7 +1,16 @@
 (defproject figwheel-main-example "0.1.0-SNAPSHOT"
   :description "A generic fullstack Clojure/ClojureScript example project for figwheel-main."
   :url "https://github.com/jordan-yee/figwheel-main-example.git"
+
   :dependencies [[org.clojure/clojure "1.10.0"]]
+
   :main ^:skip-aot figwheel-main-example.core
+  :source-paths ["src/clj" "src/cljs"]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :aliases {;; Use figwheel.main.api connection instead for vim integration
+            "fig" ["trampoline" "run" "-m" "figwheel.main"]}
+
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.516"]
+                                  [com.bhauman/figwheel-main "0.2.0"]]}
+             :uberjar {:aot :all}})
