d8b1f477c099faa48426e9076da91fc81ac46810
Jordan Yee <jyee@qanaq.net>
2020-05-01 14:10:37 -0600

    Update project info in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject figwheel-main-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A generic fullstack Clojure/ClojureScript example project for figwheel-main."
+  :url "https://github.com/jordan-yee/figwheel-main-example.git"
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot figwheel-main-example.core
   :target-path "target/%s"
