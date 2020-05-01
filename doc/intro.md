b02e456e014086669e06426f550ef2c3b2a5203f
Jordan Yee <jyee@qanaq.net>
2020-05-01 12:17:26 -0600

    Add lein new app files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to figwheel-main-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/figwheel_main_example/core.clj
@@ -0,0 +1,7 @@
+(ns figwheel-main-example.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
